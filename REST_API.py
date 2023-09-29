09c5e434bceb852b0a8029238cb86d6509a9e816
poberttester <epam.2@mail.ru>
2023-09-29 23:55:36 +0400

    Homework first review

--- REST_API.py
+++ REST_API.py
@@ -1,5 +1,5 @@
-# Написать тест с использованием pytest и requests, в котором:#
-# Адрес сайта, имя пользователя и пароль хранятся в config.yaml#
+# Написать тест с использованием pytest и requests, в котором:
+# Адрес сайта, имя пользователя и пароль хранятся в config.yaml
 # conftest.py содержит фикстуру авторизации по адресу
 # https://test-stand.gb.ru/gateway/login с передачей параметров “username" и "password" и возвращающей токен авторизации
 # Тест с использованием DDT проверяет наличие поста
@@ -13,21 +13,39 @@ from getpass import getpass
 import requests as requests
 import yaml
 
-with open('config.yaml', 'r')as f:
+with open('config.yaml', 'r') as f:
     conf = yaml.safe_load(f)
 
 
-def get_token():
+
+def get_token() -> str:
     response = requests.post(url=conf['url'], data={'username': conf['username'], 'password': conf['password']})
     return response.json()['token']
 
-def get(token: str):
+
+def get(token: str) -> str:
     response = requests.get(conf["url_posts"],
                             headers={"X-Auth-Token": token},
                             params={"owner": "notMe"})
     return response.json()
 
 
+
+def create_post(url: str, token: str, body: dict) -> int:
+    response = requests.post(url=url, headers={"X-Auth-Token": token}, data=body)
+    return response.status_code
+
+
+# Задание Условие: Добавить в задание с REST API ещё один тест, в котором создаётся новый пост, а потом проверяется
+# его наличие на сервере по полю «описание».
+
+# Подсказка: создание поста выполняется запросом к https://test-stand.gb.ru/api/posts с передачей параметров title,
+# description, content.
+
+
 if __name__ == '__main__':
-    temp = get_token()
-    print(get(temp))
+    #create_post(conf['url_posts'], get_token(), DATA)
+    t= get_token()
+    temp=get_post(conf['url_posts'], t, {'description': 'Description'})
+    print(temp)
+    print(t)
--- test.py
+++ test.py
@@ -2,15 +2,24 @@ import pytest
 import yaml
 
 from REST_API import get
+from REST_API import create_post
 
 with open('config.yaml', 'r') as f:
     conf = yaml.safe_load(f)
+DATA = {'title': 'Title', 'description': 'Description111', 'content': 'Сontent'}
 
 def test_step1(get_token):
     result = get(get_token)
     lst = result['data']
     lst_id = [el["id"] for el in lst]
     assert conf['id'] in lst_id
+
+def test_step2(get_token):
+    create_post(conf["url_posts"], get_token, DATA)
+    result = get(get_token)
+    lst = result['data']
+    lst_id = [el['description'] for el in lst]
+    assert DATA['description'] in lst_id
 
 
 if __name__ == '__main__':
